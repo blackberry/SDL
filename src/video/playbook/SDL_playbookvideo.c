blackberry/SDL

commit fce830fff2dacc81e9ea20e5e5af06d9584164b7

Added dynamic window and buffer size calculation based on the hardware and the sizes provided by the app.

src/video/playbook/SDL_playbookvideo.c
@@ -63,6 +63,7 @@
 #include <string.h>
 #include <errno.h>
 #include <stdio.h>
+#include <math.h>
 
 #define PLAYBOOKVID_DRIVER_NAME "playbook"
 
@@ -325,7 +326,41 @@ SDL_Surface *PLAYBOOK_SetVideoMode(_THIS, SDL_Surface *current,
 
 	int rc;
 	int format = 0;
+
+
+#ifdef __STRETCHED__
+
 	int sizeOfWindow[2] = {1024, 600};
+
+#else
+	int hwResolution[2];
+
+	rc = screen_get_window_property_iv(screenWindow, SCREEN_PROPERTY_SIZE, hwResolution);
+	if (rc) {
+		SDL_SetError("Cannot get resolution: %s", strerror(errno));
+		screen_destroy_window(screenWindow);
+		return NULL;
+	}
+
+	float hwRatio, appRatio;
+	hwRatio = (float)hwResolution[0]/(float)hwResolution[1];
+	appRatio = (float)width/(float)height;
+
+//	int sizeOfWindow[2] = {816, 478};
+	double newResolution[2];
+	if(hwRatio > appRatio){
+		newResolution[0] = ((double)height / ((double)hwResolution[1] / (double)hwResolution[0]));
+		newResolution[1] = (double)height;
+	}else{
+		newResolution[0] = (((double)hwResolution[1] / (double)hwResolution[0]) * (double)width);
+		newResolution[1] = (double)width;
+	}
+
+	int sizeOfWindow[2];
+	sizeOfWindow[0] = (int)(ceil(newResolution[0]));
+	sizeOfWindow[1] = (int)(ceil(newResolution[1]));
+#endif
+
 	rc = screen_set_window_property_iv(screenWindow, SCREEN_PROPERTY_SIZE, sizeOfWindow);
 	if (rc) {
 		SDL_SetError("Cannot resize window: %s", strerror(errno));

commit ca8f1ab651fde3271366ebf7a550754fd2a4ce58

Fixed bug introduced in fce830fff2dacc81e9ea20e5e5af where width and height could be switched when calculating a non-stretched resolution.

src/video/playbook/SDL_playbookvideo.c
@@ -295,14 +295,18 @@ screen_window_t PLAYBOOK_CreateWindow(_THIS, SDL_Surface *current,
 {
 	screen_window_t screenWindow;
 	int rc = 0;
+	int position[2] = {0, 0};
+	int idle_mode = SCREEN_IDLE_MODE_KEEP_AWAKE; // TODO: Handle idle gracefully?
+
 	if (!_priv->screenWindow) {
+		char groupName[256];
+
 		rc = screen_create_window(&screenWindow, _priv->screenContext);
 		if (rc) {
 			SDL_SetError("Cannot create window: %s", strerror(errno));
 			return NULL;
 		}
 
-		char groupName[256] = {0};
 		snprintf(groupName, 256, "sdl-%dx%dx%d-%u", width, height, bpp, time(NULL));
 		rc = screen_create_window_group(screenWindow, groupName);
 		if (rc) {
@@ -320,7 +324,6 @@ screen_window_t PLAYBOOK_CreateWindow(_THIS, SDL_Surface *current,
 		screenWindow = _priv->screenWindow;
 	}
 
-	int position[2] = {0, 0};
 	rc = screen_set_window_property_iv(screenWindow, SCREEN_PROPERTY_POSITION, position);
 	if (rc) {
 		SDL_SetError("Cannot position window: %s", strerror(errno));
@@ -328,7 +331,6 @@ screen_window_t PLAYBOOK_CreateWindow(_THIS, SDL_Surface *current,
 		return NULL;
 	}
 
-	int idle_mode = SCREEN_IDLE_MODE_KEEP_AWAKE; // TODO: Handle idle gracefully?
 	rc = screen_set_window_property_iv(screenWindow, SCREEN_PROPERTY_IDLE_MODE, &idle_mode);
 	if (rc) {
 		SDL_SetError("Cannot disable idle mode: %s", strerror(errno));
@@ -387,8 +389,8 @@ SDL_Surface *PLAYBOOK_SetVideoMode(_THIS, SDL_Surface *current,
 		newResolution[0] = ((double)height / ((double)hwResolution[1] / (double)hwResolution[0]));
 		newResolution[1] = (double)height;
 	}else{
-		newResolution[0] = (((double)hwResolution[1] / (double)hwResolution[0]) * (double)width);
-		newResolution[1] = (double)width;
+		newResolution[0] = (double)width;
+		newResolution[1] = (((double)hwResolution[1] / (double)hwResolution[0]) * (double)width);
 	}
 
 	int sizeOfWindow[2];